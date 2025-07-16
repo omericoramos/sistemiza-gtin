7dce50f2e61e12e46c13be54d59b1649f4b25068
omericoramos <omericoramos@gmail.com>
2025-07-16 15:35:25 -0300

    feat: adicionando as rotas para o upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExtrairGtin\UploadNfeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('extrair-gtin')->group(function () {
+        Route::get('/upload', [UploadNfeController::class, 'index'])->name('extractGtin.index');
+        Route::post('/upload', [UploadNfeController::class, 'upload'])->name('extractGtin.upload');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
